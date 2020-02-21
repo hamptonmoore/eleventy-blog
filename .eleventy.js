d9ac488ab7e58820666b49517dc2068cdbcf33a9
Hampton Moore <me@hampton.pw>
2020-02-21 16:59:26 +0100

    Customize

--- .eleventy.js
+++ .eleventy.js
@@ -33,7 +33,6 @@ module.exports = function(eleventyConfig) {
     return array.slice(0, n);
   });
 
-  eleventyConfig.addCollection("tagList", require("./_11ty/getTagList"));
 
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("css");
@@ -41,7 +40,7 @@ module.exports = function(eleventyConfig) {
   /* Markdown Overrides */
   let markdownLibrary = markdownIt({
     html: true,
-    breaks: true,
+    breaks: false,
     linkify: true
   }).use(markdownItAnchor, {
     permalink: true,
